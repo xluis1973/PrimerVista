1ce3f80d7ff6f39447b6a8e18a18fdb32ed1b110
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-08-17 11:05:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PrimerVista
+ 
---
+++ src/primervista/PrimerVista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primervista;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrimerVista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Calculadora().setVisible(true);
+    }
+    
+}
